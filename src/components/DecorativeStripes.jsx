57f965c1a4deb7138b910435955064200a2d4450
Matej Minar <matej.minar@brackets.sk>
2019-10-16 23:00:28 +0200

    feat: decorative stripes component

---
+++ src/components/DecorativeStripes.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const DecorativeStripes = () => {
+  return (
+    <div className="flex">
+      <div className="h-1 flex-1 bg-purple"></div>
+      <div className="h-1 flex-1 bg-green"></div>
+      <div className="h-1 flex-1 bg-pink"></div>
+      <div className="h-1 flex-1 bg-yellow"></div>
+    </div>
+  )
+}
