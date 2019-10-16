725fcccdc4a2ff9b22e68f460f34e5e53cae2219
Matej Minar <matej.minar@brackets.sk>
2019-10-16 17:27:16 +0200

    feat: welcome section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,18 @@ import "../styles/tailwind.css"
 const LandingPage = () => (
   <>
     <SEO title="Home" />
+    <section className="max-w-4xl py-16 mx-auto">
+      <p className="text-3xl text-grey">
+        Hey. This is <span className="text-purple">Matej Minar</span>. I am a
+        <span className="text-green"> javascript engineer</span> who loves
+        building beautiful and intuitive UIs. I always like to learn new things
+        and contribute to the <span className="text-pink">open source</span>, as
+        I am very grateful for the community and owe it so much. I just moved to{" "}
+        <span className="text-yellow">Vienna</span> and I am looking for new
+        opportunities. In the past, I had the pleasure of working on products
+        for following companies:
+      </p>
+    </section>
   </>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      pink: "#b76ac4",
+      green: "#24b47e",
+      purple: "#6772e5",
+      blue: "#3297d3",
+      grey: "#32325d",
+      orange: "#F2A581",
+      yellow: "#f5be58",
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
